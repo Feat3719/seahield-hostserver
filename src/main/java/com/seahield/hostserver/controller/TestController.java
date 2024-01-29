ea38b26a98796575ffb141874c43a659281e1315
Feat <rksk8888@naver.com>
2024-01-29 12:33:00 +0900

    FEAT : DOCKER 파일 생성 후 수정

--- src/main/java/com/seahield/hostserver/controller/TestController.java
+++ src/main/java/com/seahield/hostserver/controller/TestController.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RestController
 public class TestController {
 
-    @GetMapping("/test/${index}")
+    @GetMapping("/test/{index}")
     public String TestMethod(@PathVariable int index) {
 
-        return "CI/CD Test";
+        return "CI/CD Test" + index;
     }
 
 }
