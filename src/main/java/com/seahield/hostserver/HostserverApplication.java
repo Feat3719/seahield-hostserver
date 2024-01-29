53c54def8e9fd28328c5493d9216ea444b832b39
Feat <rksk8888@naver.com>
2024-01-29 10:53:10 +0900

    CREATE : 프로젝트 최초 생성

---
+++ src/main/java/com/seahield/hostserver/HostserverApplication.java
@@ -0,0 +1,13 @@
+package com.seahield.hostserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seahield/hostserver/HostserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seahield.hostserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
