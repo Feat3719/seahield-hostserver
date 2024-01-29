61bc1b1f890321dc3a96dfdb85e373778309276f
Feat <rksk8888@naver.com>
2024-01-29 11:20:51 +0900

    TEST : controller test

---
+++ src/main/java/com/seahield/hostserver/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.seahield.hostserver.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test/${index}")
+    public String TestMethod(@PathVariable int index) {
+
+        return "CI/CD Test";
+    }
+
+}
