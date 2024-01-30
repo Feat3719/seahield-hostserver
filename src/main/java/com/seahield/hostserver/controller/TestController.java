bf0c04796ac40774ace4fb8f39dd338bae7134bc
Feat <rksk8888@naver.com>
2024-01-30 10:39:58 +0900

    test

--- src/main/java/com/seahield/hostserver/controller/TestController.java
+++ src/main/java/com/seahield/hostserver/controller/TestController.java
@@ -9,7 +9,7 @@ public class TestController {
 
     @GetMapping("/test/{index}")
     public String TestMethod(@PathVariable int index) {
-
+        System.out.println("asdf");
         return "CI/CD Test" + index;
     }
 
